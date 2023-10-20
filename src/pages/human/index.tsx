0d36d2cf71aadcf7802ab0564a04130c95fcf38d
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-10-21 01:49:33 +0900

    ok

---
+++ src/pages/human/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+import { Button } from '~/components/ui/button'
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      human
+      <Button variant="outline">hai</Button>
+    </div>
+  )
+}
+
+export default Page
--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -8,6 +8,9 @@ export const pagesPath = {
     },
     $url: (url?: { query?: OptionalQuery0, hash?: string }) => ({ pathname: '/article' as const, query: url?.query, hash: url?.hash })
   },
+  "human": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/human' as const, hash: url?.hash })
+  },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
