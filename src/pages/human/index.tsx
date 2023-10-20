bb07c7ddad8eb7ee86ddde6e49cb11b51a58bb45
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-10-21 02:47:22 +0900

    ok

--- src/pages/human/index.tsx
+++ src/pages/human/index.tsx
@@ -1,31 +1,31 @@
+import useAspidaSWR from '@aspida/swr'
 import { NextPage } from 'next'
 import Layout from '~/components/Layout'
 import { Button } from '~/components/ui/button'
+import { apiClient } from '~/utils/apiClient'
 
 const Page: NextPage = () => {
+  const { data } = useAspidaSWR(apiClient.human)
+
   return (
     <Layout>
       <div className="flex items-center justify-between border-b pb-2">
         <h1 className="text-2xl">メンバー一覧</h1>
         <Button>新規作成</Button>
       </div>
-      <ul className="mt-5 divide-y">
-        <li className="flex items-center gap-3 py-2">
-          <span className="w-[100px]">田中 太郎</span>
-          <span className="w-[50px]">26 歳</span>
-          <Button className="ml-auto">編集</Button>
-        </li>
-        <li>
-          <span>田中 太郎</span>
-          <span>26 歳</span>
-          <Button>編集</Button>
-        </li>
-        <li>
-          <span>田中 太郎</span>
-          <span>26 歳</span>
-          <Button>編集</Button>
-        </li>
-      </ul>
+      {data ? (
+        <ul className="mt-5 divide-y">
+          {data.humans.map((human) => (
+            <li key={human.id} className="flex items-center gap-3 py-2">
+              <span className="w-[100px]">田中 太郎</span>
+              <span className="w-[50px]">26 歳</span>
+              <Button className="ml-auto">編集</Button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>loading...</div>
+      )}
     </Layout>
   )
 }
